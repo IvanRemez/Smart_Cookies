2f92498e6211c982e3ee88b3381a53e22bf78194
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-02-23 10:20:25 -0500

    added Anagram solution

--- src/main/java/Solutions/week6/Q1_ValidAnagram.java
+++ src/main/java/Solutions/week6/Q1_ValidAnagram.java
@@ -21,13 +21,48 @@ Output: false
 Please, test your solution here : https://leetcode.com/problems/valid-anagram/
  */
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Q1_ValidAnagram {
 
     public static void main(String[] args) {
-
+        System.out.println(isAnagram_Zehra("anagram", "nagaram")); // true
+        System.out.println(isAnagram_Zehra("rat", "car")); // false
+        System.out.println(isAnagram_Zehra("Buckethead", "DeathCubeK")); // true
+        System.out.println(isAnagram_Zehra("", " ")); // false
+        System.out.println(isAnagram_Zehra2("anagram", "nagaram")); // true
+        System.out.println(isAnagram_Zehra2("rat", "car")); // false
+        System.out.println(isAnagram_Zehra2("Buckethead", "DeathCubeK")); // true
+        System.out.println(isAnagram_Zehra2("", " ")); // false
 
     }
 
     // *ADD YOUR SOLUTIONS HERE*
+    public static boolean isAnagram_Zehra(String s, String t) {
+        s = s.toLowerCase();
+        t = t.toLowerCase();
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+        if (Arrays.equals(sArray, tArray)) {
+            return true;
+        }
+        return false;
+    }
+    public static boolean isAnagram_Zehra2(String test, String original) {
+        return Stream.of(test.toLowerCase().split(""))
+                .sorted()
+                .collect(Collectors.joining())
+                .equals(Stream.of(original.toLowerCase().split(""))
+                        .sorted()
+                        .collect(Collectors.joining()));
+    }
 
 }
