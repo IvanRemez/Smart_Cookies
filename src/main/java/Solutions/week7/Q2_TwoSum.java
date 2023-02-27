f559254691e55d4cb3e7ed9885cc1ba952644901
Ivan_Cydeo <remezivan1@gmail.com>
2023-02-27 10:02:13 -0500

    week7 tasks added - Ivan

---
+++ src/main/java/Solutions/week7/Q2_TwoSum.java
@@ -0,0 +1,26 @@
+package Solutions.week7;
+
+/*
+Question-2 Two Sum
+
+Given an array of integers nums and an integer target, return the two numbers such that they add up to target.
+Assume that each input would have exactly one solution, and you should not use the same element twice.
+You can return the answer in any order.
+No solution will return empty array.
+Please, use two pointer strategy to solve this problem
+(Hint : you should sort the input first).
+
+Example:
+Input: nums = [16, 3, 11, 5, 15], target = 8
+Output: [3, 5]
+Explanation: Because nums[1] + nums[3] => 3 + 5 = 8, we return [3, 5].
+Optional additional task: Try to return original indices of the pair numbers.
+ */
+
+public class Q2_TwoSum {
+
+    public static void main(String[] args) {
+
+
+    }
+}
