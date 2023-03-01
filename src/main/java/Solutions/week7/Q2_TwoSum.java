e5a159f9bf09007bc5fe39746036024809e3648e
sopiokvantaliani <skvantaliani@gmail.com>
2023-02-28 21:01:45 -0500

    Week7 Q2

--- src/main/java/Solutions/week7/Q2_TwoSum.java
+++ src/main/java/Solutions/week7/Q2_TwoSum.java
@@ -28,7 +28,35 @@ public class Q2_TwoSum {
 
         System.out.println(Arrays.toString(Oleksii_TwoSum(arr, target)));
 
+
+        int [] num = {3, 5, 7, 1, 2, 4};
+        System.out.println(Arrays.toString(sum_Sopo(num, 5)));
+        System.out.println(Arrays.toString(sum_Sopo(num, 0)));
+
     }
+
+    public static int [] sum_Sopo (int [] num, int target) {
+        Arrays.sort(num);
+        int left = 0;
+        int right = num.length-1;
+
+
+        while (left<right){
+
+            int sum = num[left]+num[right];
+
+            if (sum==target){
+                return new int[] {left, right};
+            }else if (sum<target) {
+                left++; //starts checking from next index
+            }else {
+                right--; //coming down one by one until match;
+            }
+        }
+
+        return new int[] {}; //if the match isn't found returns empty array
+    }
+
 
     static int[] Oleksii_TwoSum(int[] arr, int target) {
 
