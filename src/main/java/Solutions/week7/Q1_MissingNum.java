4c9703004965f80cfce0a5e57dde6aef3536e27b
owner <mahirsarac89@gmail.com>
2023-03-01 22:49:47 -0500

    Q1 Mahir`s Solution

--- src/main/java/Solutions/week7/Q1_MissingNum.java
+++ src/main/java/Solutions/week7/Q1_MissingNum.java
@@ -37,6 +37,7 @@ public class Q1_MissingNum {
         int[] nums = {9, 6, 4, 2, 3, 5, 7, 0, 1};
 
         //int[] nums = {0,1};
+        System.out.println(missingNum_Mahir(nums));
 
         System.out.println(Oleksii_MissingNum(nums));
     
@@ -94,6 +95,16 @@ public class Q1_MissingNum {
             }
         }
         return array[array.length - 1] + 1;
+    }
+    public static  int missingNum_Mahir(int[]nums){
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i!=nums[i]){
+                return i;
+            }
+        }
+        return nums.length;
     }
 
 }
