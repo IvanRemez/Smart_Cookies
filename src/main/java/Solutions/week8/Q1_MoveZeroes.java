a97327e2a4959ddcdffc80b8cbb35257e9974794
Ivan_Cydeo <remezivan1@gmail.com>
2023-03-06 15:13:55 -0500

    week8 tasks added

---
+++ src/main/java/Solutions/week8/Q1_MoveZeroes.java
@@ -0,0 +1,31 @@
+package Solutions.week8;
+
+/*
+Question-1 Move Zeroes
+Week-08 Algo Questions
+Given an integer array nums, move all 0's to the end of it while maintaining the relative
+order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+Input: nums = [0, 1, 0, 3, 12]
+Example 2: Input: nums = [0]
+Constraints:
+• 1 <= nums.length <= 104
+• -231 <= nums[i] <= 231 - 1
+Output: [1, 3, 12, 0, 0] Output: [0]
+Follow up: Could you minimize the total number of operations done?
+Please, test your solution here: https://leetcode.com/problems/move-zeroes/
+ */
+
+public class Q1_MoveZeroes {
+
+    public static void main(String[] args) {
+
+        // TESTS HERE
+
+    }
+
+    // SOLUTIONS HERE
+
+}
