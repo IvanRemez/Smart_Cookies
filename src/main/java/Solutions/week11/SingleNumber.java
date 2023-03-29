b413a09d378d9c146501f2358216d20d1d478c5d
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-03-28 22:15:41 -0400

    added week 11-Question 1

--- src/main/java/Solutions/week11/SingleNumber.java
+++ src/main/java/Solutions/week11/SingleNumber.java
@@ -1,38 +1,61 @@
 package Solutions.week11;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SingleNumber {
 
-  public static void main(String[] args) {
-    int[] nums = null;
+    public static void main(String[] args) {
+        int[] nums = null;
 
-    nums = new int[]{2, 2, 1}; // expected result: 1
-    System.out.println("input nums: " + Arrays.toString(nums));
-    System.out.println("result: " + singleNumber_Maria(nums)); //todo instead of dummy method, call your method from here
-    System.out.println("****************************");
+        nums = new int[]{2, 2, 1}; // expected result: 1
+        System.out.println("input nums: " + Arrays.toString(nums));
+        System.out.println("result: " + singleNumber_Maria(nums)); //todo instead of dummy method, call your method from here
+        System.out.println("****************************");
 
-    nums = new int[]{2, 2, 4, 5, 5}; // expected result: 4
-    System.out.println("input nums: " + Arrays.toString(nums));
-    System.out.println("result: " + singleNumber_Maria(nums)); //todo instead of dummy method, call your method from here
-    System.out.println("****************************");
+        nums = new int[]{2, 2, 4, 5, 5}; // expected result: 4
+        System.out.println("input nums: " + Arrays.toString(nums));
+        System.out.println("result: " + singleNumber_Maria(nums)); //todo instead of dummy method, call your method from here
+        System.out.println("****************************");
 
-    nums = new int[]{9}; // expected result: 9
-    System.out.println("input nums: " + Arrays.toString(nums));
-    System.out.println("result: " + singleNumber_Maria(nums)); //todo instead of dummy method, call your method from here
-    System.out.println("****************************");
-  }
+        nums = new int[]{9}; // expected result: 9
+        System.out.println("input nums: " + Arrays.toString(nums));
+        System.out.println("result: " + singleNumber_Maria(nums)); //todo instead of dummy method, call your method from here
+        System.out.println("****************************");
 
-  static int dummySolution(int[] nums) {
-    //just for method blueprint
-    return 0;
-  }
-  public static int singleNumber_Maria(int[] nums) {
+        int[] nums1 = {2, 2, 1};
+        int[] nums2 = {4, 1, 2, 1, 2};
+        int[] nums3 = {1};
 
-    int result = 0;
-    for (int i:nums) {
-      result ^=i;
+        System.out.println(singleNumber_Zehra(nums1)); // 1
+        System.out.println(singleNumber_Zehra(nums2)); // 4
+        System.out.println(singleNumber_Zehra(nums3)); // 1
     }
-    return result;
-  }
+
+    static int dummySolution(int[] nums) {
+        //just for method blueprint
+        return 0;
+    }
+
+    public static int singleNumber_Maria(int[] nums) {
+
+        int result = 0;
+        for (int i : nums) {
+            result ^= i;
+        }
+        return result;
+    }
+
+    public static int singleNumber_Zehra(int[] nums) {
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.remove(nums[i])) {
+                set.add(nums[i]);
+            }
+        }
+        return set.stream().findAny().orElseThrow();
+    }
 }
