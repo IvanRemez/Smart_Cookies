9ff61ffd2dac31fda1a36be9aec1aaab1d3a35d6
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-03-28 22:48:22 -0400

    edited week 11-Question 1

--- src/main/java/Solutions/week11/SingleNumber.java
+++ src/main/java/Solutions/week11/SingleNumber.java
@@ -12,23 +12,24 @@ public class SingleNumber {
         nums = new int[]{2, 2, 1}; // expected result: 1
         System.out.println("input nums: " + Arrays.toString(nums));
         System.out.println("result: " + singleNumber_Maria(nums)); //todo instead of dummy method, call your method from here
+        System.out.println("result: " + singleNumber_Zehra(nums));
         System.out.println("****************************");
 
         nums = new int[]{2, 2, 4, 5, 5}; // expected result: 4
         System.out.println("input nums: " + Arrays.toString(nums));
         System.out.println("result: " + singleNumber_Maria(nums)); //todo instead of dummy method, call your method from here
+        System.out.println("result: " + singleNumber_Zehra(nums));
         System.out.println("****************************");
 
         nums = new int[]{9}; // expected result: 9
         System.out.println("input nums: " + Arrays.toString(nums));
         System.out.println("result: " + singleNumber_Maria(nums)); //todo instead of dummy method, call your method from here
+        System.out.println("result: " + singleNumber_Zehra(nums));
         System.out.println("****************************");
 
-        int[] nums1 = {2, 2, 1};
         int[] nums2 = {4, 1, 2, 1, 2};
         int[] nums3 = {1};
 
-        System.out.println(singleNumber_Zehra(nums1)); // 1
         System.out.println(singleNumber_Zehra(nums2)); // 4
         System.out.println(singleNumber_Zehra(nums3)); // 1
     }
