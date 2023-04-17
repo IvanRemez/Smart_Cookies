7ffc45a4e317e0f6ae6c66c62627097f4cb2939c
Ivan_Cydeo <remezivan1@gmail.com>
2023-04-17 10:30:22 -0400

    week14 update

---
+++ src/main/java/Solutions/week14/ElementsGreaterThanTheirRight.java
@@ -0,0 +1,30 @@
+package Solutions.week14;
+/*
+Question-1 Find All Elements Greater Than Their Right
+
+Given an unsorted integer array, return all greater elements than all elements present to their right using Stack.
+Example:
+Input: [10, 4, 6, 3, 5]
+Output: [10, 6, 5]
+The elements that are greater than all elements to their right are 10, 6, and 5. 10>4 & 10>6 & 10>3 & 10>5
+
+class Main{
+// Function to print all elements which are greater than all
+// elements present to their right
+public List<Integer> find(int[] arr){
+    }
+}
+Please, try to test your solution via JUnit.
+ */
+
+public class ElementsGreaterThanTheirRight {
+
+    public static void main(String[] args) {
+
+        // TEST HERE
+
+    }
+
+    // SOLUTIONS HERE
+
+}
---
+++ src/main/java/Solutions/week14/PalindromeLinkedList.java
@@ -0,0 +1,30 @@
+package Solutions.week14;
+/*
+Question-2 Palindrome Linked List
+
+Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+Example 1:
+Input: head = [1, 2, 2, 1] Output: true
+Example 2:
+Input: head = [1, 2] Output: false
+
+Constraints:
+• The number of nodes in the list is in the range [1, 10^5].
+• 0 <= Node.val <= 9
+Follow up: Could you do it in O(n) time and O(1) space?
+
+Please, try to test your solution via JUnit and also test your solution here:
+https://leetcode.com/problems/palindrome-linked-list/
+ */
+
+public class PalindromeLinkedList {
+
+    public static void main(String[] args) {
+
+        // TEST HERE
+
+    }
+
+    // SOLUTIONS HERE
+
+}
