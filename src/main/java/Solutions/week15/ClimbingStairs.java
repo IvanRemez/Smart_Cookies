fdfc5fdd78f061c4b7cd866e7da6ef3457875bae
Ivan_Cydeo <remezivan1@gmail.com>
2023-04-24 10:39:09 -0400

    week15 update

---
+++ src/main/java/Solutions/week15/ClimbingStairs.java
@@ -0,0 +1,38 @@
+package Solutions.week15;
+/*
+Question-1 Climbing Stairs
+
+Simba has a number of staircases in his house, and he likes to climb each staircase
+1 or 2 steps at a time.
+It takes n steps to reach the top. In how many distinct ways can he climb to the top?
+
+Example 1:
+Input: n = 2 Output: 2
+Explanation: There are two ways to climb to the top. 1. 1 step + 1 step
+2. 2 steps
+Example 2:
+Input: n = 3 Output: 3
+Explanation: There are three ways to climb to the top. 1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+Constraints:
+• 1 <= n <= 45
+
+Follow-up: What if he climbs each staircase 1, 2 or 3 steps at a time?
+Please, try to test your solution via JUnit and also test your solution here:
+https://leetcode.com/problems/climbing-stairs/
+Hint: Think about Fibonacci numbers. How would you find the n’th number in Fibonacci series?
+ */
+
+public class ClimbingStairs {
+
+    public static void main(String[] args) {
+
+        // TEST HERE
+
+    }
+
+    // SOLUTIONS HERE
+
+}
