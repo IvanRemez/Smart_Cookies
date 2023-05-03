c38e1b851557d0e017a9efb39be677c15f2e5207
Ivan_Cydeo <remezivan1@gmail.com>
2023-05-03 10:49:08 -0400

    week16 update

---
+++ src/main/java/Solutions/week16/AlterCase.java
@@ -0,0 +1,30 @@
+package Solutions.week16;
+/*
+Question-2 Alter Case
+
+Given a path of input file which includes sentence(s), write a program that reads alternates the
+case of every alphabetic character, starting with uppercase. Spaces and non-alphabetical characters
+should be added to the final output as is, i.e. they should not be taken into account when alternating
+between upper/lowercase. Input: Your program should read lines from file as a standard input. Each
+line contains a sentence. Assume all characters are ASCII.
+
+Output: For each sentence from standard input, return a string which contains a sentence in each line
+such that the first character is uppercase, the next character is lowercase and so on.
+Test 1
+Test Input: We are the world Expected Output: We ArE tHe WoRlD
+Test 2
+Test Input: this is some code
+Expected Output: ThIs Is SoMe CoDe Please, try to test your solution via JUnit.
+ */
+
+public class AlterCase {
+
+    public static void main(String[] args) {
+
+        // TEST HERE
+
+    }
+
+    // SOLUTIONS HERE
+
+}
