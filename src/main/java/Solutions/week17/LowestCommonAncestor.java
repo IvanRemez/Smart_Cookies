6dcd67d57423365d7dd3a4af61356ad850d25e4b
Ivan_Cydeo <remezivan1@gmail.com>
2023-05-09 14:23:52 -0400

    week17 update

---
+++ src/main/java/Solutions/week17/LowestCommonAncestor.java
@@ -0,0 +1,30 @@
+package Solutions.week17;
+/*
+Question-1 Find the Lowest Common Ancestor
+Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two
+given nodes in the BST. According to the definition of LCA on Wikipedia: “The lowest
+common ancestor is defined between two nodes p and q as the lowest node in T that has
+both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+(CHECK PDF FOR TREE EXAMPLES)
+
+Constraints:
+The number of nodes in the tree is in the range [2, 105]. -109 <= Node.val <= 109
+All Node.val are unique.
+p != q
+p and q will exist in the BST.
+
+https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree
+ */
+
+public class LowestCommonAncestor {
+
+    public static void main(String[] args) {
+
+        // TEST HERE
+
+    }
+
+    // SOLUTIONS HERE
+
+}
