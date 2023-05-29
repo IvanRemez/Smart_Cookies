cb95e1b36abec052587f10016a4f775d6e990620
Ivan_Cydeo <remezivan1@gmail.com>
2023-05-29 12:54:38 -0400

    week20 update

---
+++ src/main/java/Solutions/week12/RemoveDuplicatesFromLL.java
@@ -0,0 +1,33 @@
+package Solutions.week12;
+
+/*
+Question-2 Remove Duplicates From Linked List
+
+You're given the head of a Singly Linked List whose nodes are in sorted order with respect to their values.
+Write a function that returns a modified version of the Linked List that doesn't contain any nodes with duplicate values.
+The Linked List should be modified in place (i.e., you shouldn't create a brand-new list), and the modified Linked List
+should still have its nodes sorted with respect to their values. Each LinkedList node has an integer value as well as a
+next node pointing to the next node in the list or to None / null if it's the tail of the list.
+
+Example 1:
+Input: head = [1,1,2,3,3] Output: [1,2,3]
+Example:
+Input: [1, 1, 3, 4, 4, 4, 5, 6, 6] // the head node with value 1 Output: [1, 3, 4, 5, 6] // the head node with value 1
+
+Constraints:
+• The number of nodes in the list is in the range [0, 300].
+• -100 <= Node.val <= 100
+• The list is guaranteed to be sorted in ascending order.
+Please, try to test your solution via JUnit and also test your solution here:
+https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+ */
+
+public class RemoveDuplicatesFromLL {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
